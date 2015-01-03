49f498dd53ce9c09b7b2391d709c5be1d19503e6
Nate Cook <natecook@gmail.com>
2015-01-03 05:26:12 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+// includes
+var fs = require('fs'),
+    parser = require('./lib/swiftdoc-parser'),
+    builder = require('./lib/swiftdoc-builder');
+    
+// command-line options
+var yargs = require('yargs')
+        .usage('Usage: $0 [options] [file names...]')
+        .help('help')
+        .alias('?', 'help')
+        .alias('h', 'help')
+        .options('json-only', {
+            type: 'boolean',
+            describe: 'Output parsed headers as JSON only',
+        })
+        .options('config', {
+            type: 'string',
+            describe: 'Location of a configuration file',
+        })
+        .options('output-dir', {
+            type: 'string',
+            describe: 'Output directory',
+            default : './output',
+        })
+        .options('url-prefix', {
+            type: 'string',
+            describe: 'URL prefix for generated links',
+            default : '/',
+        }),
+    argv = yargs.argv;
+
+// read config file if one was given
+var config = { };
+if (argv['config']) {
+    config = JSON.parse(fs.readFileSync(argv.config));
+}
+
+// find list of source files either in config file or as command line params
+var sources = config['source-files'] || argv._ || [];
+if (sources.length == 0) {
+    console.log('No input files given!\nPlease list input files in a configuration file or on the command line.\n');
+    console.log(yargs.help());
+    return;
+}
+
+// parse each source file, building up a parsed data object
+var parsedData = sources.reduce(function(previous, current) {
+    var data = fs.readFileSync(current, 'utf8');
+    return parser.parse(data, previous);
+}, null);
+
+// finalize the parsing; this hoists nested types up to the top level and builds reverse-inheritance
+parsedData = parser.finalize(parsedData);
+
+// output JSON blob and stop if that's the request
+if (argv['json-only']) {
+    console.log(JSON.stringify(parsedData));
+    return;
+}
+
+// set output-related configuration items
+builder.setOutputDir(config['output-dir'] || argv['output-dir']);
+builder.setURLPrefix(config['url-prefix'] || argv['url-prefix']);
+
+// build output and log the resulting message    
+if (builder.build(parsedData)) {
+    
+}
+
+
---
+++ test/test-parse.js
@@ -0,0 +1,171 @@
+// includes
+var fs = require('fs'),
+    expect = require('chai').expect,
+    parser = require('../lib/swiftdoc-parser');
+
+// 'data/swift-lang-6.2b1.txt'
+var dataFilename = 'test/data/ok.txt';
+var data = fs.readFileSync(dataFilename, 'utf8');
+var parsedData = parser.parse(data, null);
+parsedData = parser.finalize(parsedData);
+
+Array.prototype.firstElementNamed = function(name) {
+    var result = this.filter(function(e) { return e.name == name });
+    return result.length == 0 ? null : result[0];
+}
+
+describe('Parser', function() {
+    describe('operators', function() {
+
+        var plus        = parsedData.operators[0];
+        var increment   = parsedData.operators[1];
+        var equality    = parsedData.operators[2];
+
+        it('count', function() {
+            expect(parsedData.operators.length).to.equal(3);
+        });
+        
+        it('+ functions', function() {
+            expect(plus.functions.length).to.equal(4);
+        });
+        
+        it('names', function() {
+            expect(plus.name).to.equal('+');
+            expect(increment.name).to.equal('prefix ++');
+            expect(equality.name).to.equal('==');
+        });
+        
+    });
+    
+    describe('type aliases', function() {
+    
+        it('count', function() {
+            expect(parsedData.aliases.length).to.equal(7);
+        });
+    
+        it('typing', function() {
+            expect(parsedData.aliases.firstElementNamed('CLong').type).to.equal('Int');
+        });
+        
+    });
+    
+    describe('properties', function() {
+        
+        it('count', function() {
+            expect(parsedData.properties.length).to.equal(1);
+        });
+        
+    });
+
+    describe('functions', function() {
+    
+        it('count', function() {
+            expect(parsedData.functions.length).to.equal(11);
+        });
+    
+        var partitionFunction = parsedData.functions.firstElementNamed('partition');
+        it('`partition` parameters', function() {
+            expect(partitionFunction).to.exist;
+
+            expect(partitionFunction.params.length).to.equal(3);
+
+            expect(partitionFunction.params[2].name).to.equal('isOrderedBefore');
+            expect(partitionFunction.params[2].types).to.contain('Bool');
+            expect(partitionFunction.params[2].types).to.contain('Element');
+            expect(partitionFunction.params[2].types).to.contain('Generator');
+        });
+        
+        var withUnsafeFunction = parsedData.functions.firstElementNamed('withUnsafeMutablePointers');
+        it('`withUnsafeMutablePointers` function', function() {
+            expect(withUnsafeFunction).to.exist;
+
+            expect(withUnsafeFunction.generic.line).to.equal('A0, A1, A2, Result');
+            expect(withUnsafeFunction.params.length).to.equal(4);
+            expect(withUnsafeFunction.params[3].type).to.equal('(UnsafeMutablePointer<A0>, UnsafeMutablePointer<A1>, UnsafeMutablePointer<A2>) -> Result');
+        });
+    });
+
+    describe('types + protocols', function() {
+        
+        it('count', function() {
+            expect(Object.keys(parsedData.types).length).to.equal(41);
+        });
+        
+        describe('`Array`', function() {
+        
+            var arrayData = parsedData.types['Array'];
+            
+            it('type', function() {
+                expect(arrayData.kind).to.equal('struct');
+            });
+            
+            it('init count', function() {
+                expect(arrayData.inits.length).to.equal(5);
+            });
+        
+            it('property count', function() {
+                expect(arrayData.properties.length).to.equal(7);
+            });
+        
+            it('function count', function() {
+                expect(arrayData.functions.length).to.equal(15);
+            });
+        
+            it('subscript count', function() {
+                expect(arrayData.subscripts.length).to.equal(2);
+            });
+        
+            it('alias count', function() {
+                expect(arrayData.aliases.length).to.equal(2);
+            });
+        
+            it('inheritance', function() {
+                expect(arrayData.inherits).to.contain('ArrayLiteralConvertible');
+                expect(arrayData.inherits).to.contain('MutableCollectionType');
+                expect(arrayData.inherits).to.contain('Sliceable');
+            });
+            
+            it('comments', function() {
+                expect(arrayData.comment).to.contain('is deferred until the\nfirst element access');
+                expect(arrayData.comment).to.contain('  var b = a');
+            });
+        });
+        
+        describe('`_CollectionType`', function() {
+
+            var collectionTypeData = parsedData.types['_CollectionType'];
+            
+            it('type', function() {
+                expect(collectionTypeData.kind).to.equal('protocol');
+            });
+            
+            it('init count', function() {
+                expect(collectionTypeData.inits.length).to.equal(0);
+            });
+        
+            it('property count', function() {
+                expect(collectionTypeData.properties.length).to.equal(2);
+            });
+        
+            it('function count', function() {
+                expect(collectionTypeData.functions.length).to.equal(0);
+            });
+        
+            it('subscript count', function() {
+                expect(collectionTypeData.subscripts.length).to.equal(1);
+            });
+        
+            it('alias count', function() {
+                expect(collectionTypeData.aliases.length).to.equal(2);
+            });
+        
+            it('inheritance', function() {
+                expect(collectionTypeData.inherits).to.contain('_SequenceType');
+                expect(collectionTypeData.inherited).to.contain('CollectionType');
+            });
+            
+        });
+        
+        
+    });
+});
